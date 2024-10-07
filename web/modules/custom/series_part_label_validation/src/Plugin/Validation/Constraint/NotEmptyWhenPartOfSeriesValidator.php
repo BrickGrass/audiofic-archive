71a49c98479d0ea3181dd75719c91f5feeaf83ee
BrickGrass <49255229+BrickGrass@users.noreply.github.com>
2024-10-07 17:35:00 +0100

    Added custom module to validate field_part_label is present on works in series

---
+++ web/modules/custom/series_part_label_validation/src/Plugin/Validation/Constraint/NotEmptyWhenPartOfSeriesValidator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\series_part_label_validation\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates the NotEmptyWhenPartOfSeries constraint.
+ */
+class NotEmptyWhenPartOfSeriesValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($value, Constraint $constraint) {
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $entity = $this->context->getRoot()->getValue();
+    if ($entity->hasField("field_series") && $entity->get("field_series") != NULL) {
+      $this->context->addViolation($constraint->needsValue, [
+        '%field' => $value->getFieldDefinition()->getLabel(),
+      ]);
+    }
+  }
+}
