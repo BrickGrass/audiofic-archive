750fd5eb22e9cfe97a47bbfe5076b947514a1984
BrickGrass <49255229+BrickGrass@users.noreply.github.com>
2024-11-09 00:23:34 +0000

    Create add new tag button/link

---
+++ web/modules/custom/aa_utils/js/add_new_tag.js
@@ -0,0 +1,15 @@
+(function ($) {
+  $(document).ready(function() {
+    let fandom = $(".form-item--field-fandom2-target-id .claro-autocomplete");
+    $(fandom).append("<a aria-label='Open a new tab to add a new fandom tag' href='/admin/structure/taxonomy/manage/fandom/add' target='_blank' class='add-entity-link add-entity-link-margin-left'>+</a>");
+    $(fandom).addClass("autocomplete-with-add-link")
+
+    let relationship = $(".form-item--field-relationship-target-id .claro-autocomplete");
+    $(relationship).append("<a aria-label='Open a new tab to add a new relationship tag' href='/admin/structure/taxonomy/manage/relationship/add' target='_blank' class='add-entity-link add-entity-link-margin-left'>+</a>");
+    $(relationship).addClass("autocomplete-with-add-link")
+
+    let language = $(".form-item--field-language-target-id .claro-autocomplete");
+    $(language).append("<a aria-label='Open a new tab to add a new language tag' href='/admin/structure/taxonomy/manage/language/add' target='_blank' class='add-entity-link add-entity-link-margin-left'>+</a>");
+    $(language).addClass("autocomplete-with-add-link")
+  })
+})(jQuery);
