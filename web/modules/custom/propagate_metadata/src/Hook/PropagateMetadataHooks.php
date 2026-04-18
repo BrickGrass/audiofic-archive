93389ba4924ac769ef3aba11590a9da498b9bcfa
BrickGrass <49255229+BrickGrass@users.noreply.github.com>
2026-04-18 17:25:54 +0100

    Implement archive locking & overhaul node permissions to use node_grants api rather than the access_by_reference module

--- web/modules/custom/propagate_metadata/src/Hook/PropagateMetadataHooks.php
+++ web/modules/custom/propagate_metadata/src/Hook/PropagateMetadataHooks.php
@@ -2,7 +2,22 @@
 
 namespace Drupal\propagate_metadata\Hook;
 
+use Drupal\aa_utils\Service\AudioficUtils;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\NodeInterface;
+use Drupal\user\Entity\User;
+
+// View permission realm & options.
+define('AUDIOFIC_REALM_VIEW', 'audiofic_realm_view');
+define('AUDIOFIC_GRANT_PUBLIC', 0);
+define('AUDIOFIC_GRANT_ARCHIVE_LOCKED', 1);
+define('AUDIOFIC_GRANT_ARCHIVE_UNPUBLISHED', 2);
+
+// Edit/Update/Delete permission realm & admin grant,
+// all other grants are based on owner roles.
+define('AUDIOFIC_REALM_EDIT', 'audiofic_realm_edit');
+define('AUDIOFIC_GRANT_ADMIN', 0);
 
 /**
  * Class PropagateMetadataHooks.
@@ -10,6 +25,10 @@ use Drupal\Core\Hook\Attribute\Hook;
  * Ensures that users are not saved with invalid data/settings.
  */
 class PropagateMetadataHooks {
+
+  public function __construct(
+    protected AudioficUtils $utils,
+  ) {}
 
   /**
    * Implements hook_entity_type_alter().
@@ -22,6 +41,120 @@ class PropagateMetadataHooks {
       $entity_types['user']->addConstraint('UserNotAdminAndPodficcer');
       $entity_types['user']->addConstraint('AttributionTagNotAlreadyInUse');
     }
+  }
+
+  /**
+   * Implements hook_node_grants().
+   *
+   * Defines the access permissions a user has, depending on the operation
+   * that is being checked (eg: view, update, delete, etc).
+   */
+  #[Hook('node_grants')]
+  public function nodeGrants(AccountInterface $account, string $operation): array {
+    $grants = [];
+    $roles = $account->getRoles();
+
+    if ($operation == 'view') {
+      if (in_array('administrator', $roles)) {
+        $grants[AUDIOFIC_REALM_VIEW] = [
+          AUDIOFIC_GRANT_PUBLIC,
+          AUDIOFIC_GRANT_ARCHIVE_LOCKED,
+          AUDIOFIC_GRANT_ARCHIVE_UNPUBLISHED,
+        ];
+      } elseif (in_array('authenticated', $roles)) {
+        $grants[AUDIOFIC_REALM_VIEW] = [
+          AUDIOFIC_GRANT_PUBLIC,
+          AUDIOFIC_GRANT_ARCHIVE_LOCKED,
+        ];
+      } else {
+        $grants[AUDIOFIC_REALM_VIEW] = [
+          AUDIOFIC_GRANT_PUBLIC,
+        ];
+      }
+    } else {
+      if (in_array('administrator', $roles)) {
+        $grants[AUDIOFIC_REALM_EDIT] = [
+          AUDIOFIC_GRANT_ADMIN,
+        ];
+      } elseif (in_array('authenticated', $roles)) {
+        $user = User::load($account->id());
+        if (!empty($user)) {
+          $user_tags = array_column($user->get('field_reader_name')->getValue(), 'target_id');
+          $grants[AUDIOFIC_REALM_EDIT] = $user_tags;
+        }
+      }
+    }
+
+    return $grants;
+  }
+
+  /**
+   * Implements hook_node_access_records().
+   *
+   * Defines the access grants that a user needs to access this node.
+   */
+  #[Hook('node_access_records')]
+  public function nodeAccess(NodeInterface $node): array {
+    $grants = [];
+
+    // Unpublished nodes can only be viewed by admins.
+    // Archive locked nodes can only be viewed by logged in users.
+    // Published & non-locked nodes can be viewed by anyone.
+    if (!$node->isPublished()) {
+      $grants[] = [
+        'realm' => AUDIOFIC_REALM_VIEW,
+        'gid' => AUDIOFIC_GRANT_ARCHIVE_UNPUBLISHED,
+        'grant_view' => 1,
+        'grant_update' => 0,
+        'grant_delete' => 0,
+        'priority' => 0,
+      ];
+    } elseif ($this->utils->isNodeArchiveLocked($node)) {
+      $grants[] = [
+        'realm' => AUDIOFIC_REALM_VIEW,
+        'gid' => AUDIOFIC_GRANT_ARCHIVE_LOCKED,
+        'grant_view' => 1,
+        'grant_update' => 0,
+        'grant_delete' => 0,
+        'priority' => 0,
+      ];
+    } else {
+      $grants[] = [
+        'realm' => AUDIOFIC_REALM_VIEW,
+        'gid' => AUDIOFIC_GRANT_PUBLIC,
+        'grant_view' => 1,
+        'grant_update' => 0,
+        'grant_delete' => 0,
+        'priority' => 0,
+      ];
+    }
+
+    // Admins have edit access to any/all nodes.
+    $grants[] = [
+      'realm' => AUDIOFIC_REALM_EDIT,
+      'gid' => AUDIOFIC_GRANT_ADMIN,
+      'grant_view' => 1,
+      'grant_update' => 1,
+      'grant_delete' => 1,
+      'priority' => 0,
+    ];
+
+    // Non-admin users only have edit access to nodes they own.
+    if ($node->hasField('field_owner')) {
+      $owners = array_column($node->get('field_owner')->getValue(), 'target_id');
+      foreach ($owners as $owner_id) {
+        $grants[] = [
+          'realm' => AUDIOFIC_REALM_EDIT,
+          'gid' => $owner_id,
+          'grant_view' => 1,
+          'grant_update' => 1,
+          'grant_delete' => 1,
+          'priority' => 0,
+        ];
+      }
+    }
+
+    return $grants;
   }
 
 }
