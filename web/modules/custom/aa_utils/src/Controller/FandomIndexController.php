7f1f6b21bf8f26ac33ad370b12f1a04fcb206455
BrickGrass <49255229+BrickGrass@users.noreply.github.com>
2026-05-05 21:44:04 +0100

    optimise queries

--- web/modules/custom/aa_utils/src/Controller/FandomIndexController.php
+++ web/modules/custom/aa_utils/src/Controller/FandomIndexController.php
@@ -67,45 +67,7 @@ class FandomIndexController extends ControllerBase {
    */
   public function fandomIndex() {
     $root_fandom_ids = $this->tagUtils->getRootFandomIds();
-
-    $query = $this->database->query(
-      "WITH base_data AS (
-      SELECT taxonomy_term_field_data.name AS term_name,
-             taxonomy_term_field_data.tid AS term_id,
-             COUNT(DISTINCT node__field_fandom2.entity_id) AS total_work_count,
-             taxonomy_term__parent.parent_target_id AS root_fandom
-      FROM {taxonomy_term_field_data}
-      INNER JOIN {taxonomy_term__parent}
-      ON taxonomy_term_field_data.tid = taxonomy_term__parent.entity_id
-      INNER JOIN {taxonomy_term__field_canonicity}
-      ON taxonomy_term_field_data.tid = taxonomy_term__field_canonicity.entity_id
-      LEFT JOIN {taxonomy_term__field_canon_sibling}
-      ON taxonomy_term_field_data.tid = taxonomy_term__field_canon_sibling.field_canon_sibling_target_id
-      INNER JOIN {node__field_fandom2}
-      ON taxonomy_term_field_data.tid = node__field_fandom2.field_fandom2_target_id
-      OR taxonomy_term__field_canon_sibling.entity_id = node__field_fandom2.field_fandom2_target_id
-      INNER JOIN {node_field_data}
-      ON node__field_fandom2.entity_id = node_field_data.nid
-      WHERE taxonomy_term_field_data.vid = 'fandom' AND
-            taxonomy_term__field_canonicity.field_canonicity_value = 'canon' AND
-            node__field_fandom2.bundle IN ('legacy_work', 'work') AND
-            node_field_data.status = 1 AND
-            taxonomy_term__parent.parent_target_id IN (:root_fandom_ids[])
-      GROUP BY taxonomy_term_field_data.tid,
-               taxonomy_term__parent.parent_target_id
-      ),
-      base_data_with_rank AS (
-      SELECT *, row_number() OVER (PARTITION BY root_fandom ORDER BY total_work_count DESC) AS row_no
-      FROM base_data
-      )
-      SELECT *
-      FROM base_data_with_rank
-      WHERE row_no <= 5
-      ORDER BY root_fandom, total_work_count DESC;",
-      [':root_fandom_ids[]' => $root_fandom_ids],
-    );
-    $top_5_lists = [];
-    foreach ($query->fetchAll() as $row) {
+    foreach ($this->getAllTop5Fandoms($root_fandom_ids) as $row) {
       $top_5_lists[$row->root_fandom][] = $row;
     }
 
@@ -150,7 +112,6 @@ class FandomIndexController extends ControllerBase {
     if (empty($taxonomy_term)) {
       throw new NotFoundHttpException('No taxonomy term provided!');
     }
-
     if (
       !$this->tagUtils->isTagCanonicityAware($taxonomy_term) ||
       $taxonomy_term->get('field_canonicity')->value != 'canonical_root_fandom'
@@ -173,34 +134,8 @@ class FandomIndexController extends ControllerBase {
       return $this->redirect('aa_utils.fandom_page', ['taxonomy_term' => $taxonomy_term->id()]);
     }
 
-    $query = $this->database->query(
-      "SELECT taxonomy_term_field_data.name AS term_name,
-              taxonomy_term_field_data.tid AS term_id,
-              COUNT(DISTINCT node__field_fandom2.entity_id) AS work_count
-       FROM {taxonomy_term_field_data}
-       INNER JOIN {taxonomy_term__parent}
-       ON taxonomy_term_field_data.tid = taxonomy_term__parent.entity_id
-       INNER JOIN {taxonomy_term__field_canonicity}
-       ON taxonomy_term_field_data.tid = taxonomy_term__field_canonicity.entity_id
-       LEFT JOIN {taxonomy_term__field_canon_sibling}
-       ON taxonomy_term_field_data.tid = taxonomy_term__field_canon_sibling.field_canon_sibling_target_id
-       INNER JOIN {node__field_fandom2}
-       ON taxonomy_term_field_data.tid = node__field_fandom2.field_fandom2_target_id
-       OR taxonomy_term__field_canon_sibling.entity_id = node__field_fandom2.field_fandom2_target_id
-       INNER JOIN {node_field_data}
-       ON node__field_fandom2.entity_id = node_field_data.nid
-       WHERE taxonomy_term_field_data.vid = 'fandom' AND
-             taxonomy_term__field_canonicity.field_canonicity_value = 'canon' AND
-             node__field_fandom2.bundle IN ('legacy_work', 'work') AND
-             node_field_data.status = 1 AND
-             taxonomy_term__parent.parent_target_id = :root_fandom_id
-       GROUP BY taxonomy_term_field_data.tid
-       ORDER BY taxonomy_term_field_data.name;",
-      [':root_fandom_id' => $taxonomy_term->id()],
-    );
-
     $fandom_index = [];
-    foreach ($query->fetchAll() as $row) {
+    foreach ($this->getFandomWorkTotals($taxonomy_term->id()) as $row) {
       $fandom_index[mb_strtolower(mb_substr($row->term_name, 0, 1, 'utf-8'), 'UTF-8')][] = $row;
     }
 
@@ -226,6 +161,135 @@ class FandomIndexController extends ControllerBase {
     }
 
     return $taxonomy_term->getName();
+  }
+
+  /**
+   * Performs a database query that fetches the top 5 fandoms for each category.
+   *
+   * Fetches the following fields: term name, term id, total work count,
+   * root fandom & index in top 5.
+   */
+  private function getAllTop5Fandoms(array $root_fandom_ids): array {
+    $query = $this->database->query(
+      "WITH term_data AS (
+        SELECT taxonomy_term_field_data.name AS term_name,
+               taxonomy_term_field_data.tid AS term_id,
+               node__field_fandom2.entity_id AS work_id,
+               taxonomy_term__parent.parent_target_id AS root_fandom
+        FROM {taxonomy_term_field_data}
+        INNER JOIN {taxonomy_term__parent}
+        ON taxonomy_term_field_data.tid = taxonomy_term__parent.entity_id
+        INNER JOIN {taxonomy_term__field_canonicity}
+        ON taxonomy_term_field_data.tid = taxonomy_term__field_canonicity.entity_id
+        INNER JOIN {node__field_fandom2}
+        ON taxonomy_term_field_data.tid = node__field_fandom2.field_fandom2_target_id
+        INNER JOIN {node_field_data}
+        ON node__field_fandom2.entity_id = node_field_data.nid
+        WHERE taxonomy_term_field_data.vid = 'fandom' AND
+              taxonomy_term__field_canonicity.field_canonicity_value = 'canon' AND
+              node__field_fandom2.bundle IN ('legacy_work', 'work') AND
+              node_field_data.status = 1 AND
+              taxonomy_term__parent.parent_target_id IN (:root_fandom_ids[])
+        UNION
+        SELECT taxonomy_term_field_data.name AS term_name,
+               taxonomy_term_field_data.tid AS term_id,
+               node__field_fandom2.entity_id AS work_id,
+               taxonomy_term__parent.parent_target_id AS root_fandom
+        FROM {taxonomy_term_field_data}
+        INNER JOIN {taxonomy_term__parent}
+        ON taxonomy_term_field_data.tid = taxonomy_term__parent.entity_id
+        INNER JOIN {taxonomy_term__field_canonicity}
+        ON taxonomy_term_field_data.tid = taxonomy_term__field_canonicity.entity_id
+        LEFT JOIN {taxonomy_term__field_canon_sibling}
+        ON taxonomy_term_field_data.tid = taxonomy_term__field_canon_sibling.field_canon_sibling_target_id
+        INNER JOIN {node__field_fandom2}
+        ON taxonomy_term__field_canon_sibling.entity_id = node__field_fandom2.field_fandom2_target_id
+        INNER JOIN {node_field_data}
+        ON node__field_fandom2.entity_id = node_field_data.nid
+        WHERE taxonomy_term_field_data.vid = 'fandom' AND
+              taxonomy_term__field_canonicity.field_canonicity_value = 'canon' AND
+              node__field_fandom2.bundle IN ('legacy_work', 'work') AND
+              node_field_data.status = 1 AND
+              taxonomy_term__parent.parent_target_id IN (:root_fandom_ids[])
+      ),
+      term_data_grouped AS (
+        SELECT term_data.term_name,
+               term_data.term_id,
+               term_data.root_fandom,
+               COUNT(DISTINCT term_data.work_id) AS total_work_count
+        FROM term_data
+        GROUP BY term_data.term_id, term_data.root_fandom
+      ),
+      term_data_with_rank AS (
+        SELECT *,
+               row_number() OVER (PARTITION BY root_fandom ORDER BY total_work_count DESC) AS row_no
+        FROM term_data_grouped
+      )
+      SELECT *
+      FROM term_data_with_rank
+      WHERE row_no <= 5
+      ORDER BY root_fandom, total_work_count DESC;",
+      [':root_fandom_ids[]' => $root_fandom_ids],
+    );
+    return $query->fetchAll();
+  }
+
+  /**
+   * Performs a database query that fetches all fandoms in a category.
+   *
+   * Fetches the following fields: term name, term id & total work count.
+   * The results are returned in alphabetical order.
+   */
+  private function getFandomWorkTotals(int $root_fandom): array {
+    $query = $this->database->query(
+      "WITH term_data AS (
+        SELECT taxonomy_term_field_data.name AS term_name,
+               taxonomy_term_field_data.tid AS term_id,
+               node__field_fandom2.entity_id AS work_id
+        FROM {taxonomy_term_field_data}
+        INNER JOIN {taxonomy_term__parent}
+        ON taxonomy_term_field_data.tid = taxonomy_term__parent.entity_id
+        INNER JOIN {taxonomy_term__field_canonicity}
+        ON taxonomy_term_field_data.tid = taxonomy_term__field_canonicity.entity_id
+        INNER JOIN {node__field_fandom2}
+        ON taxonomy_term_field_data.tid = node__field_fandom2.field_fandom2_target_id
+        INNER JOIN {node_field_data}
+        ON node__field_fandom2.entity_id = node_field_data.nid
+        WHERE taxonomy_term_field_data.vid = 'fandom' AND
+              taxonomy_term__field_canonicity.field_canonicity_value = 'canon' AND
+              node__field_fandom2.bundle IN ('legacy_work', 'work') AND
+              node_field_data.status = 1 AND
+              taxonomy_term__parent.parent_target_id = :root_fandom_id
+        UNION
+        SELECT taxonomy_term_field_data.name AS term_name,
+               taxonomy_term_field_data.tid AS term_id,
+               node__field_fandom2.entity_id AS work_id
+        FROM {taxonomy_term_field_data}
+        INNER JOIN {taxonomy_term__parent}
+        ON taxonomy_term_field_data.tid = taxonomy_term__parent.entity_id
+        INNER JOIN {taxonomy_term__field_canonicity}
+        ON taxonomy_term_field_data.tid = taxonomy_term__field_canonicity.entity_id
+        LEFT JOIN {taxonomy_term__field_canon_sibling}
+        ON taxonomy_term_field_data.tid = taxonomy_term__field_canon_sibling.field_canon_sibling_target_id
+        INNER JOIN {node__field_fandom2}
+        ON taxonomy_term__field_canon_sibling.entity_id = node__field_fandom2.field_fandom2_target_id
+        INNER JOIN {node_field_data}
+        ON node__field_fandom2.entity_id = node_field_data.nid
+        WHERE taxonomy_term_field_data.vid = 'fandom' AND
+              taxonomy_term__field_canonicity.field_canonicity_value = 'canon' AND
+              node__field_fandom2.bundle IN ('legacy_work', 'work') AND
+              node_field_data.status = 1 AND
+              taxonomy_term__parent.parent_target_id = :root_fandom_id
+      )
+      SELECT term_data.term_id,
+             term_data.term_name,
+             COUNT(DISTINCT term_data.work_id) AS work_count
+      FROM term_data
+      GROUP BY term_data.term_id
+      ORDER BY term_data.term_name;",
+      [':root_fandom_id' => $root_fandom],
+    );
+    return $query->fetchAll();
   }
 
 }
